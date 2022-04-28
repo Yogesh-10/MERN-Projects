c997e29ca0afd0d0a83e7f180e957eff2949d475
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-04-28 20:37:50 +0530

    block and unblock user routes

--- blog-mern-advanced/server/routes/users/userRoutes.js
+++ blog-mern-advanced/server/routes/users/userRoutes.js
@@ -10,6 +10,8 @@ const {
   updateUserPassword,
   followUser,
   unfollowUser,
+  blockUser,
+  unBlockUser,
 } = require('../../controllers/users/userController');
 const authMiddleware = require('../../middlewares/auth/authMiddleware');
 
@@ -23,6 +25,9 @@ userRoutes.put('/password', authMiddleware, updateUserPassword);
 
 userRoutes.put('/follow', authMiddleware, followUser);
 userRoutes.put('/unfollow', authMiddleware, unfollowUser);
+
+userRoutes.put('/block-user/:id', authMiddleware, blockUser);
+userRoutes.put('/unblock-user/:id', authMiddleware, unBlockUser);
 
 userRoutes.get('/profile/:id', authMiddleware, userProfile);
 userRoutes.put('/:id', authMiddleware, updateUser);
