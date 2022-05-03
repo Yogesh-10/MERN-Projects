e51511632e153c2020f3ca66048aaeaed1a3e4bb
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-05-03 19:44:43 +0530

    base url added + tailwind config changed

--- blog-mern-advanced/client/src/App.js
+++ blog-mern-advanced/client/src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AddNewCategory from './components/Categories/AddNewCategory';
+import CategoryList from './components/Categories/CategoryList';
+import UpdateCategory from './components/Categories/UpdateCategory';
 import HomePage from './components/HomePage/HomePage';
 import Navbar from './components/Navigation/Navbar';
 import Login from './components/Users/Login/Login';
@@ -9,6 +12,9 @@ const App = () => {
     <BrowserRouter>
       <Navbar />
       <Switch>
+        <Route exact path='/update-category/:id' component={UpdateCategory} />
+        <Route exact path='/add-category' component={AddNewCategory} />
+        <Route exact path='/category-list' component={CategoryList} />
         <Route exact path='/' component={HomePage} />
         <Route exact path='/register' component={Register} />
         <Route exact path='/login' component={Login} />
