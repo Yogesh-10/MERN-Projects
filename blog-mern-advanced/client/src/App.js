f7c19cd18d6e7a244581c67a6d372205b678f5bb
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-05-05 00:00:05 +0530

    App routes

--- blog-mern-advanced/client/src/App.js
+++ blog-mern-advanced/client/src/App.js
@@ -2,11 +2,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AddNewCategory from './components/Categories/AddNewCategory';
 import CategoryList from './components/Categories/CategoryList';
 import UpdateCategory from './components/Categories/UpdateCategory';
+import UpdateComment from './components/Comments/UpdateComment';
 import HomePage from './components/HomePage/HomePage';
 import Navbar from './components/Navigation/Navbar';
 import AdminRoute from './components/Navigation/ProtectedRoutes/AdminRoute';
 import PrivateProtectRoute from './components/Navigation/ProtectedRoutes/PrivateProtectRoute';
 import CreatePost from './components/Posts/CreatePost';
+import PostDetails from './components/Posts/PostDetails';
+import PostsList from './components/Posts/PostsList';
+import UpdatePost from './components/Posts/UpdatePost';
 import Login from './components/Users/Login/Login';
 import Register from './components/Users/Register/Register';
 
@@ -20,8 +24,20 @@ const App = () => {
           path='/update-category/:id'
           component={UpdateCategory}
         />
+        <PrivateProtectRoute
+          exact
+          path='/update-post/:id'
+          component={UpdatePost}
+        />
         <PrivateProtectRoute exact path='/create-post' component={CreatePost} />
+        <PrivateProtectRoute
+          exact
+          path='/update-comment/:id'
+          component={UpdateComment}
+        />
         <AdminRoute exact path='/add-category' component={AddNewCategory} />
+        <Route exact path='/posts' component={PostsList} />
+        <Route exact path='/posts/:id' component={PostDetails} />
         <AdminRoute exact path='/category-list' component={CategoryList} />
         <Route exact path='/' component={HomePage} />
         <Route exact path='/register' component={Register} />
--- blog-mern-advanced/client/src/utils/LoadingComponent.js
+++ blog-mern-advanced/client/src/utils/LoadingComponent.js
@@ -1,15 +1,9 @@
-import React from "react";
-import { css } from "@emotion/react";
-import RiseLoader from "react-spinners/CircleLoader";
-
-//css
-const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: red;
-`;
 const LoadingComponent = () => {
-  return <RiseLoader color="red" loading={true} css={override} />;
+  return (
+    <div className='loadingSpinnerContainer'>
+      <div className='loadingSpinner'></div>
+    </div>
+  );
 };
 
 export default LoadingComponent;
