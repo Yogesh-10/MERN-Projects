69a8dae834d4c93f5a44cb67000cd35943a0ff52
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-07-14 23:52:12 +0530

    Landing Page

---
+++ job-tracking-app/client/src/pages/Landing.js
@@ -0,0 +1,29 @@
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            Job <span>Tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
+            mollitia error obcaecati nobis unde quisquam odio numquam impedit,
+            vitae aliquid.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
